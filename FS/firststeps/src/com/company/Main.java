321165757f6c2a7456db0036adda70b4b2795cfc
ivs13 <ivs13@mail.ru>
2014-08-29 16:43:06 +0300

    ++

--- FS/firststeps/src/com/company/Main.java
+++ FS/firststeps/src/com/company/Main.java
@@ -1,11 +1,9 @@
 package com.company;
 
-import System.out.*;
-
 public class Main {
 
     public static void main(String[] args)
     {
-	    System.out.println("Hello world!");
+        System.out.println("Hello world!++");
     }
 }
