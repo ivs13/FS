7e7fc6f6c089537071b75ea7029a89b40d056963
ivs13 <ivs13@mail.ru>
2014-08-29 19:05:24 +0300

    Begin of LinkedList realisation

--- FS/firststeps/src/com/company/LinkedList.java
+++ FS/firststeps/src/com/company/LinkedList.java
@@ -1,6 +1,32 @@
 package com.company;
 
 public class LinkedList<T> {
-    LinkedList head = null;
-    String value;
+
+    private class Node {
+        private T data;
+        private Node next = null;
+
+        private Node(T data) {
+            this.data = data;
+        }
+
+        private T getData() {
+            return data;
+        }
+
+        private void setData(T data) {
+            this.data = data;
+        }
+
+        private Node getNext() {
+            return next;
+        }
+
+        private void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
+    private Node head;
+
 }
