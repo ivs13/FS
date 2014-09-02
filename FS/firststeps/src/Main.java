a851f684e6bd8909f403580929765d605dc84444
ivs13 <ivs13>
2014-09-02 22:06:51 +0300

    home

--- FS/firststeps/src/Main.java
+++ FS/firststeps/src/Main.java
@@ -1,7 +1,27 @@
+import my.LinkedList.*;
+
 public class Main {
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws NodeNotFoundException
     {
-        System.out.println("Hello world!++");
+        LinkedList<String> ls = new LinkedList<String>();
+        ls.PrintYourself();
+        ls.InsertAtBegin("ABC"); ls.PrintYourself();
+        ls.InsertAtEnd("XYZ"); ls.PrintYourself();
+        ls.InsertAtBegin("111"); ls.PrintYourself();
+        ls.InsertAtEnd("***"); ls.PrintYourself();
+        ls.InsertAfter("DDD", "ABC"); ls.PrintYourself();
+        ls.InsertBefore("KKK", "XYZ"); ls.PrintYourself();
+        ls.InsertBefore(null, "KKK"); ls.PrintYourself();
+        ls.InsertBefore(null, null); ls.PrintYourself();
+        ls.Delete("DDD"); ls.PrintYourself();
+        ls.Delete("111"); ls.PrintYourself();
+        ls.Delete("***"); ls.PrintYourself();
+        ls.Delete(null); ls.PrintYourself();
+        try {
+            ls.Delete("GGG");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
